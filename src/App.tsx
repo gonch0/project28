ffca3d107715493692c7ead0d7ce6a6993e1ccd6
Ivan Goncharov <givan1010@gmail.com>
2023-02-12 20:55:20 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, { Suspense } from 'react';
+import {
+    Link,
+    Route,
+    Routes,
+} from 'react-router-dom';
+import './styles/index.scss';
+import { classNames } from './helpers/classNames';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async.';
+
+import { useTheme } from './theme/useTheme';
+
+const App = () => {
+
+    const {
+        theme,
+        toggleTheme,
+    } = useTheme();
+
+    return (
+        // <div className={`app ${theme}`}>
+        <div className={classNames('app',
+            {},
+            [theme],
+        )}
+        >
+            <button
+                onClick={toggleTheme}
+            >
+                toggle theme
+            </button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route
+                        path={'/'}
+                        element={<MainPageAsync />}
+                    />
+                    <Route
+                        path={'/about'}
+                        element={<AboutPageAsync />}
+                    />
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function someFn(arg0: number): string {
+    console.log('RANDOM FUNCTION 12312');
+
+    return '';
+};
