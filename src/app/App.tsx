dd5a88a9d4204d8408cf29875311d608f57194a5
Ivan Goncharov <givan1010@gmail.com>
2023-02-18 18:42:52 +0500

    routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,9 @@
-import React, { Suspense } from 'react';
-import {
-    Link,
-    Route,
-    Routes,
-} from 'react-router-dom';
+import AppRouter from 'app/providers/router/ui/AppRouter';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 
 const App = () => {
 
@@ -18,10 +13,12 @@ const App = () => {
     } = useTheme();
 
     return (
-        <div className={classNames('app',
-            {},
-            [theme],
-        )}
+        <div
+            className={classNames(
+                'app',
+                {},
+                [theme],
+            )}
         >
             <button
                 onClick={toggleTheme}
@@ -30,18 +27,7 @@ const App = () => {
             </button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route
-                        path={'/'}
-                        element={<MainPage />}
-                    />
-                    <Route
-                        path={'/about'}
-                        element={<AboutPage />}
-                    />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
